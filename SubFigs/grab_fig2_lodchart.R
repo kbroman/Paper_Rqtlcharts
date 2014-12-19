d4ed4ce1960c5598f02af90868eb3f981c4a0eb0
Karl Broman <kbroman@gmail.com>
2014-12-19 16:33:13 -0600

    grab_fig2_lodchart: revise box to capture for lodchart part of fig 2

--- SubFigs/grab_fig2_lodchart.R
+++ SubFigs/grab_fig2_lodchart.R
@@ -1,7 +1,8 @@
 library(webshot)
 
 # C: lodchart
-webshot("fig2_pieces.html", file="fig2_lodchart.png", selector="#lodchart",
-        eval="casper.then(function() { this.mouse.move(185,887) })")
-#system("convert -flatten fig2_heatmap.png tmp.png")
-#system("convert -crop 380x400+10+10 tmp.png fig2_heatmap.png")
+webshot("fig2_lodchart.html", file="tmp.png", selector="#lodchart",
+        vwidth=700, vheight=400,
+        eval="casper.then(function() { this.mouse.move(155,232.5) })")
+system("convert -flatten tmp.png fig2_lodchart.png")
+unlink("tmp.png")
