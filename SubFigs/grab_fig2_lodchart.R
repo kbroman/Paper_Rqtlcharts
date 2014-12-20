5fd5dd46f9016019e3504003a52cf86bcc1bf187
Karl Broman <kbroman@gmail.com>
2014-12-19 19:43:10 -0600

    cosmetic change to grab_fig2_lodchart.R

--- SubFigs/grab_fig2_lodchart.R
+++ SubFigs/grab_fig2_lodchart.R
@@ -1,6 +1,6 @@
 library(webshot)
 
-# C: lodchart
+# B: lodchart
 webshot("fig2_lodchart.html", file="tmp.png", selector="#lodchart",
         vwidth=700, vheight=400,
         eval="casper.then(function() { this.mouse.move(155,232.5) })")
